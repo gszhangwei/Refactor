e0509bbbc8857b9860f77806a43fbdb6269d7852
gszhangwei <279230582@qq.com>
2023-08-07 14:41:35 +0800

    0000 [wei] refactor the code

--- src/HandleDataRange.java
+++ src/HandleDataRange.java
@@ -1,12 +1,11 @@
 import level.OffsetLevel;
-import factory.DateRangeProcessor;
 import model.DateRange;
 
 import java.util.Date;
 
 public class HandleDataRange {
     public void handleDataRange(String offsetLevel, Date startDatetime, int offsetValue, Date endDatetime, int filterLevel, String funcLevel, Date selectedDate, int startDateMonthAfterOffset) {
-        DateRange dateRange = DateRangeProcessor.getDateRange(OffsetLevel.fromString(offsetLevel), startDatetime, offsetValue, endDatetime, filterLevel, funcLevel, selectedDate, startDateMonthAfterOffset);
+        DateRange dateRange = DateRange.getDateRange(OffsetLevel.fromString(offsetLevel), startDatetime, offsetValue, endDatetime, filterLevel, funcLevel, selectedDate, startDateMonthAfterOffset);
         dateRange.offset();
     }
 }
--- src/model/DateRange.java
+++ src/model/DateRange.java
@@ -1,8 +1,26 @@
 package model;
 
+import level.OffsetLevel;
 import lombok.Data;
+
+import java.util.Date;
 
 @Data
 public abstract class DateRange {
     public abstract void offset();
+
+    public static DateRange getDateRange(OffsetLevel offsetLevel, Date startDatetime, int offsetValue, Date endDatetime, int filterLevel, String funcLevel, Date selectedDate, int startDateMonthAfterOffset) {
+        switch (offsetLevel) {
+            case YEARLY_OFFSET:
+                return new YearlyDateRange(startDatetime, offsetValue, endDatetime);
+            case QUARTERLY_OFFSET:
+                return new QuarterlyDateRange(startDatetime, offsetValue, endDatetime, filterLevel, funcLevel, selectedDate);
+            case MONTHLY_OFFSET:
+                return new MonthlyDateRange(startDatetime, offsetValue, endDatetime, filterLevel, funcLevel, selectedDate, startDateMonthAfterOffset);
+            case DAILY_OFFSET:
+                return new DailyDateRange(startDatetime, offsetValue, endDatetime, filterLevel, funcLevel, selectedDate, startDateMonthAfterOffset);
+            default:
+                throw new IllegalArgumentException("Invalid offset level");
+        }
+    }
 }
