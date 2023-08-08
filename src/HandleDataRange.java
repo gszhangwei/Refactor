5b4afb660d32a559e61ff0b4e74fefa612bda5bc
Wiilie Zhang <wwdzhang@WeideMacBook-Pro.local>
2023-08-08 10:30:25 +0800

    0000 [wei] update the code

--- src/HandleDataRange.java
+++ src/HandleDataRange.java
@@ -1,3 +1,4 @@
+import factory.DataRangeFactory;
 import level.OffsetLevel;
 import model.DateRange;
 
@@ -5,7 +6,7 @@ import java.util.Date;
 
 public class HandleDataRange {
     public void handleDataRange(String offsetLevel, Date startDatetime, int offsetValue, Date endDatetime, int filterLevel, String funcLevel, Date selectedDate, int startDateMonthAfterOffset) {
-        DateRange dateRange = DateRange.getDateRange(OffsetLevel.fromString(offsetLevel), startDatetime, offsetValue, endDatetime, filterLevel, funcLevel, selectedDate, startDateMonthAfterOffset);
+        DateRange dateRange = DataRangeFactory.getDateRange(OffsetLevel.fromString(offsetLevel), startDatetime, offsetValue, endDatetime, filterLevel, funcLevel, selectedDate, startDateMonthAfterOffset);
         dateRange.offset();
     }
 }
