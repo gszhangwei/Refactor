b8cce2b683ef69b423b79ce1ac1f3c7bdc81e2da
gszhangwei <279230582@qq.com>
2023-08-08 10:08:27 +0800

    0000 [wei] refactor the code

---
+++ src/factory/DataRangeFactory.java
@@ -0,0 +1,27 @@
+package factory;
+
+import level.OffsetLevel;
+import model.DailyDateRange;
+import model.DateRange;
+import model.MonthlyDateRange;
+import model.QuarterlyDateRange;
+import model.YearlyDateRange;
+
+import java.util.Date;
+
+public class DataRangeFactory {
+    public static DateRange getDateRange(OffsetLevel offsetLevel, Date startDatetime, int offsetValue, Date endDatetime, int filterLevel, String funcLevel, Date selectedDate, int startDateMonthAfterOffset) {
+        switch (offsetLevel) {
+            case YEARLY_OFFSET:
+                return new YearlyDateRange(startDatetime, offsetValue, endDatetime);
+            case QUARTERLY_OFFSET:
+                return new QuarterlyDateRange(startDatetime, offsetValue, endDatetime, filterLevel, funcLevel, selectedDate);
+            case MONTHLY_OFFSET:
+                return new MonthlyDateRange(startDatetime, offsetValue, endDatetime, filterLevel, funcLevel, selectedDate, startDateMonthAfterOffset);
+            case DAILY_OFFSET:
+                return new DailyDateRange(startDatetime, offsetValue, endDatetime, filterLevel, funcLevel, selectedDate, startDateMonthAfterOffset);
+            default:
+                throw new IllegalArgumentException("Invalid offset level");
+        }
+    }
+}
